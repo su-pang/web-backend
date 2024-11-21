3a995905a4b4d33d1867a38ca6a0b54147dd15fa
박상현 <sa2840@naver.com>
2024-11-21 15:10:40 +0900

    백엔드 첫 파일

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "sa2840@naver.com"; // 받는 사람의 이메일
+    $subject = "New Message from " . $name;
+    $headers = "From: " . $email . "\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email.";
+    }
+}
+?>
